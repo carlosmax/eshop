2c4dc3c714bfabb032a7b840886f56e8b76d2c0a
Carlos Max <carlosmax.dev@hotmail.com>
2023-11-08 12:18:35 -0300

    feat: add feature to get all types

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllTypesHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllTypesHandler : IRequestHandler<GetAllTypesQuery, IList<TypeResponse>>
+    {
+        private readonly ITypeRepository _repository;
+
+        public GetAllTypesHandler(ITypeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IList<TypeResponse>> Handle(GetAllTypesQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _repository.GetAllTypes();
+            return ProductMapper.Mapper.Map<IList<TypeResponse>>(list);
+        }
+    }
+}
