576a5ec4c7a8dc20a790bda5e3f4bf990ec3c9ca
Carlos Max <carlosmax.dev@hotmail.com>
2023-11-08 12:37:11 -0300

    feat: add feature to get products by name

---
+++ Services/Catalog/Catalog.Application/Handlers/GetProductByNameHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByNameHandler : IRequestHandler<GetProductByNameQuery, IList<ProductResponse>>
+    {
+        private readonly IProductRepository _repository;
+
+        public GetProductByNameHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IList<ProductResponse>> Handle(GetProductByNameQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _repository.GetByName(request.Name);
+            return ProductMapper.Mapper.Map< IList<ProductResponse>>(list);
+        }
+    }
+}
