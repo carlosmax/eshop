d4968454bc68ef703c8c9f1bed658c07a2c37908
Carlos Max <carlosmax.dev@hotmail.com>
2023-11-08 12:11:21 -0300

    feat: add feature to get all products

---
+++ Services/Catalog/Catalog.Application/Handlers/GetAllProductsHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllProductsHandler : IRequestHandler<GetAllProductsQuery, IList<ProductResponse>>
+    {
+        private readonly IProductRepository _repository;
+
+        public GetAllProductsHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        public async Task<IList<ProductResponse>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _repository.GetAll();
+            return ProductMapper.Mapper.Map<IList<ProductResponse>>(list);
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Responses/ProductResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.Application.Responses
+{
+    public class ProductResponse
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public BrandResponse Brand { get; set; }
+        public TypeResponse Type { get; set; }
+        public decimal Price { get; set; }
+    }
+}
