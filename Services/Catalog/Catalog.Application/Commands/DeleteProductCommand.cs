ba3ba33e019d539c32c141518c84d814f40dbe8d
Carlos Max <carlosmax.dev@hotmail.com>
2023-11-08 14:56:34 -0300

    feat: add feature to delete a product

---
+++ Services/Catalog/Catalog.Application/Commands/DeleteProductCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Catalog.Application.Commands
+{
+    public class DeleteProductCommand : IRequest<bool>
+    {
+        public DeleteProductCommand(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; set; }
+    }
+}
---
+++ Services/Catalog/Catalog.Application/Handlers/DeleteProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Application.Commands;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class DeleteProductHandler : IRequestHandler<DeleteProductCommand, bool>
+    {
+        private readonly IProductRepository _repository;
+
+        public DeleteProductHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            return await _repository.Delete(request.Id);
+        }
+    }
+}
