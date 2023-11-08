f860ed5dba4abdf36343edcf93de7f4201f39ddf
Carlos Max <carlosmax.dev@hotmail.com>
2023-11-08 12:24:42 -0300

    feat: add feature to get a product by id

---
+++ Services/Catalog/Catalog.Application/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, ProductResponse>
+    {
+        private readonly IProductRepository _repository;
+
+        public GetProductByIdHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ProductResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _repository.GetById(request.Id);
+            return ProductMapper.Mapper.Map<ProductResponse>(product);
+        }
+    }
+}
