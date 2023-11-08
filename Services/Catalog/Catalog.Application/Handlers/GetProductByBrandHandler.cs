bf37880a6a6f927e988c2770f8c8d39fdafb1acf
Carlos Max <carlosmax.dev@hotmail.com>
2023-11-08 13:02:28 -0300

    feat: add feature to get products by brand

---
+++ Services/Catalog/Catalog.Application/Handlers/GetProductByBrandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByBrandHandler : IRequestHandler<GetProductByBrandQuery, IList<ProductResponse>>
+    {
+        private readonly IProductRepository _repository;
+
+        public GetProductByBrandHandler(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IList<ProductResponse>> Handle(GetProductByBrandQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _repository.GetByBrand(request.BrandName);
+            return ProductMapper.Mapper.Map<IList<ProductResponse>>(list);
+        }
+    }
+}
