0fc38f07987b04f5c511f196f31c837097f58381
Carlos Max <carlosmax.dev@hotmail.com>
2023-11-08 18:23:58 -0300

    chore: add startup file

--- Services/Catalog/Catalog.Api/Program.cs
+++ Services/Catalog/Catalog.Api/Program.cs
@@ -1,27 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace Catalog.API;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseDeveloperExceptionPage();
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.UseRouting();
-app.UseStaticFiles();
-app.MapControllers();
-
-app.Run();
---
+++ Services/Catalog/Catalog.Api/Startup.cs
@@ -0,0 +1,52 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+
+namespace Catalog.API;
+
+public class Startup
+{
+    public IConfiguration Configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddApiVersioning();
+
+        var connectionString = Configuration.GetConnectionString("CatalogDatabase");
+        services.AddHealthChecks()
+            .AddMongoDb(connectionString!, "Catalog Db Health Check", HealthStatus.Degraded);
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" }); });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        });
+    }
+}
